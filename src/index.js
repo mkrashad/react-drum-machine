0e8d9da0d3e3cc4bc3d17e4ad9e6b951182d28f6
crs2here <crs2here@gmail.com>
2019-10-01 21:24:41 -0700

    import bootstrap for app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDom from "react-dom";
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
-import App from "./App"
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
 
-ReactDom.render(<App/>, document.getElementById("root"))
+ReactDom.render(<App />, document.getElementById("root"));
